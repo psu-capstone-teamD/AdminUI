657235a1d9070e5239b684da2524e1e4e3c30031
Charles Juszczak <charles.juszczak@gmail.com>
2017-06-21 12:29:47 -0700

    Add S3 video upload + separate controller files
    -Added video upload to S3 (hit “Add to Playlist”, select video file,
    then Submit, modal does not currently close on submit but video is
    uploaded to team S3 bucket)
    -Separated controllers in to individual files

--- app/app.js
+++ app/app.js
@@ -1,30 +1,9 @@
+'use strict';
+
 // Define the AdminUI module
-var adminUI = angular.module('adminUI', []);
+var app = angular.module('adminUI', [
+    'controllers',
+    'directives'
+]);
 
-// Define the PlaylistController on the adminUI module
-adminUI.controller('PlaylistController', function PlaylistController($scope) {
-    $scope.videos = [
-
-    ];
-});
-
-// Define the MediaAssetsController on the adminUI module
-adminUI.controller('MediaAssetsController', function MediaAssetsController($scope) {
-    $scope.config = [
-
-    ];
-});
-
-// Define the ConfigController on the adminUI module
-adminUI.controller('ConfigController', function ConfigController($scope) {
-    $scope.config = [
-
-    ];
-});
-
-// Define the LiveStreamController on the adminUI module
-adminUI.controller('LiveStreamController', function ConfigController($scope) {
-    $scope.config = [
-
-    ];
-});
+var controllers = angular.module('controllers', []);
