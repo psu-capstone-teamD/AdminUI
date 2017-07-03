b87cc1b493c7464476e5fe03e2f2f2edcf059348
scott-pickthorn <scott.e.pickthorn@gmail.com>
2017-07-03 11:57:56 -0700

    adding routing for views

--- app/app.js
+++ app/app.js
@@ -3,7 +3,19 @@
 // Define the AdminUI module
 var app = angular.module('adminUI', [
     'controllers',
-    'directives'
-]);
+    'directives',
+    'ngRoute'
+]).config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "views/playlist.html"
+        })
+        .when("/livestream", {
+            templateUrl: "views/livestream.html"
+        })
+        .when("/mediaassets", {
+            templateUrl: "views/mediaassets.html"
+        })
+});
 
-var controllers = angular.module('controllers', []);
+var controllers = angular.module('controllers', '$routeProvider', [$routeProvider]);
--- app/controllers/PlaylistController.js
+++ app/controllers/PlaylistController.js
@@ -5,46 +5,47 @@ controllers.controller('PlaylistController', function PlaylistController($scope)
     ];
 
     $scope.uploadProgress = 0;
-  
+
     //Prefilled Credentials
     $scope.creds = {
         bucket: 'pdxteamdkrakatoa',
-        access_key: 'KEY',
-        secret_key: 'KEY'
+        access_key: 'AKIAJC5MDFR5KM46FQOA',
+        secret_key: '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'
+
     }
 
-    $scope.upload = function() {
+    $scope.upload = function () {
         AWS.config.update({ accessKeyId: $scope.creds.access_key, secretAccessKey: $scope.creds.secret_key });
         AWS.config.region = 'us-west-2';
         var bucket = new AWS.S3({ params: { Bucket: $scope.creds.bucket } });
-        
-        if($scope.file) {
+
+        if ($scope.file) {
             var params = { Key: $scope.file.name, ContentType: $scope.file.type, Body: $scope.file, ServerSideEncryption: 'AES256' };
 
-            bucket.putObject(params, function(err, data) {
-            if(err) {
-                toastr.error(err.message,err.code);
-                return false;
-            }
-            else {
-                // Upload Successfully Finished
-                toastr.success('File Uploaded Successfully', 'Done');
+            bucket.putObject(params, function (err, data) {
+                if (err) {
+                    toastr.error(err.message, err.code);
+                    return false;
+                }
+                else {
+                    // Upload Successfully Finished
+                    toastr.success('File Uploaded Successfully', 'Done');
 
-                // Reset The Progress Bar
-                setTimeout(function() {
-                $scope.uploadProgress = 0;
-                $scope.$digest();
-                }, 4000);
-            }
+                    // Reset The Progress Bar
+                    setTimeout(function () {
+                        $scope.uploadProgress = 0;
+                        $scope.$digest();
+                    }, 4000);
+                }
             })
-            .on('httpUploadProgress',function(progress) {
-            $scope.uploadProgress = Math.round(progress.loaded / progress.total * 100);
-            $scope.$digest();
-            });
+                .on('httpUploadProgress', function (progress) {
+                    $scope.uploadProgress = Math.round(progress.loaded / progress.total * 100);
+                    $scope.$digest();
+                });
         }
         else {
             // No File Selected
             toastr.error('Please select a file to upload');
         }
-        }
+    }
 });
