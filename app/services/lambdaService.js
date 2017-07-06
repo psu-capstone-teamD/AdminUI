79f5cbb62468437e46539a219378b1756967f4fe
Tyler Burdsall <tylerburdsall@gmail.com>
2017-07-05 18:31:00 -0700

    tests don't quite work, need to figure out how to wrangle the promise returned by $http

--- app/services/lambdaService.js
+++ app/services/lambdaService.js
@@ -1,17 +1,16 @@
 // Service to send XML data to the AWS Lambda endpoint that has been
 // set up. If an unexpected response is given, it is sent to the controller.
-adminui.service('lambdaService', ['$http', function() {
-    // Set the URL to the Lambda endpoint
-    this.endpointURL = "https://05habpy49l.execute-api.us-east-2.amazonaws.com/testing/";
-
-    this.sendBXF = function(xml) {
-        $http.post(this.endpointURL, xml).then(
-            function(success) {
-                console.log(success);
-            },
-            function(failure) {
-                console.log(failure)
-            }
-        ); // <-- config is the API authentication thing
+//angular.module('app.services',[]).service('lambdaService', ['$http', function() {
+//app.service('lambdaService', ['$http', function() {
+angular.module('adminUI', []).service('lambdaService', ['$http', lambdaService]);
+function lambdaService($http) {
+    this.sendBXF = function () {
+        $http.post("https://05habpy49l.execute-api.us-east-2.amazonaws.com/testing/", { "xml": "hey" })
+        .success(function(data) {
+            return data;
+        })
+        .error(function(data) {
+            return data;
+        });
     };
-}]);
+}
--- js/test/initialTest.spec.js
+++ js/test/initialTest.spec.js
@@ -1,19 +1,17 @@
-// Create a simple test to show that Jasmine is working
-describe("Test function", function() {
-    it("is a simple test", function() {
-        console.log("Test 1:")
-        var string = "Hello Elemental!";
-        console.log(string)
-        expect(string).toEqual("Hello Elemental!");
-    });
+'use strict';
 
-    it("ensures things are working", function() {
-        console.log("Test 2:")
-        var testNum = 0;
-        console.log(testNum)
-        testNum += 1;
-        console.log(testNum)
+describe('lambdaService', function () {
+    var lambdaService;
+    beforeEach(module('adminUI', function($provide) {
+        console.log("Beginning a test...");
+    }));
+    beforeEach(inject(function(_lambdaService_) {
+        lambdaService = _lambdaService_;
+    }));
 
-        expect(testNum).toEqual(1);
-    });
-});
+    it('should get a response', function() {
+       var result = lambdaService.sendBXF();
+        console.log(result);
+        expect(result).not.toBe(null);
+    })
+})
