98c71f201a54584dd0f24a31e743ce140f10d38a
Tyler Burdsall <tylerburdsall@gmail.com>
2017-07-25 09:47:19 -0700

    Fixes bugs, adds new features

--- app/services/S3Service.js
+++ app/services/S3Service.js
@@ -1,12 +1,13 @@
 angular.module('adminUI')
 	.service('S3Service', ['$http', '$rootScope', '$q', function($http, $rootScope, $q) {
 
+    var $scope = $rootScope;
 	var params;
 	this.bucket;
 	
 	//Prefilled Credentials
 	//Might need to be in Config, might need a Get/Set function if so.
-	var creds = {
+	$scope.creds = {
 	    bucket: 'pdxteamdkrakatoa',
 	    access_key: 'REPLACE ME',
 	    secret_key: 'REPLACE ME'
@@ -14,13 +15,15 @@ angular.module('adminUI')
 	
 
 	
+	AWS.config.update({ accessKeyId: $scope.creds.access_key, secretAccessKey: $scope.creds.secret_key });
+	AWS.config.region = 'us-west-2';
 	//Prefilled Server side encryption setting, might need to be moved into config too
 	var encryption = 'AES256'
 	
 	//Workaround to make a simple mock for the S3 object and putObject function
 	this.setBucket = function (file) {
 		params = { Key: file.name, ContentType: file.type, Body: file, ServerSideEncryption: encryption };
-		this.bucket = new AWS.S3({ params: { Bucket: creds.bucket } });
+		this.bucket = new AWS.S3({ params: { Bucket: $scope.creds.bucket } });
 	}
 	
 	var deferred = $q.defer();
@@ -32,13 +35,12 @@ angular.module('adminUI')
 			if(err) {
 				toastr.error(err.message, err.code);
 				deferred.reject(err);
-				unresolved = false;
+				//unresolved = false;
 			}
 			else
 			{
 				toastr.success('File Uploaded Successfully', 'Done');
-				deferred.resolved(data);
-				unresolved = false;
+				deferred.resolve(data);
 			}
 		})
 		.on('httpUploadProgress',function(progress) {
@@ -49,16 +51,6 @@ angular.module('adminUI')
 				$rootScope.$broadcast('')
 			}
 		});
-		
-		//Workaround for async issue, waits 2000 ms for putObject method to potentially finish
-		setTimeout(function() {
-			if(unresolved == true)
-			{
-				toastr.error("PutObject Unresolved", "putObject Unresolved");
-				deferred.reject(null);
-			}
-		}, 2000);
-	
-	return deferred.promise;
+		return deferred.promise;
     }
 }]);
