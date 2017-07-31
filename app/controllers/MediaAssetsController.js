79faf0587c4d3c9cc1425909d07d827334f55dbd
Tyler Burdsall <tylerburdsall@gmail.com>
2017-07-31 16:09:38 -0700

    change is persistent, refresh button added

--- app/controllers/MediaAssetsController.js
+++ app/controllers/MediaAssetsController.js
@@ -5,12 +5,13 @@ angular.module('adminUI')
     $scope.S3Objects = [];
 		
 	$scope.retrieveS3Objects = function(){
+        $scope.mediaAssets = mediaAssetsService.mediaAssets;
         var retrieve = S3Service.getItemsInBucket($scope.S3Objects); // Get the items in the S3 bucket
         retrieve.then(function(result) {
             $scope.S3Objects = result;
             // Check if the items exist already. If not, clear the mediaAssets and push the new results
             if(!mediaAssetsService.playlistsAreEqual($scope.S3Objects)) {
-                $scope.mediaAssets = [];
+                //$scope.mediaAssets = [];
                 $scope.S3Objects.forEach(function(obj) {
                         $scope.mediaAssets.push({thumbnail: null, title: obj.title, date: obj.date, url: obj.url});
                 });
