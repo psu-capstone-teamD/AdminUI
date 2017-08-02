1abfd86a8d7a54b01f315a951f4849a9cad6c789
Tyler Burdsall <tylerburdsall@gmail.com>
2017-08-02 14:22:20 -0700

    Automatically streams the video

--- app/controllers/LiveStreamController.js
+++ app/controllers/LiveStreamController.js
@@ -1,7 +1,15 @@
-// Define the LiveStreamController on the adminUI module
+// Define the MediaAssetsController on the adminUI module
 angular.module('adminUI')
-	.controller('LiveStreamController', function ($scope) {
-    $scope.config = [
-
-    ];
-});
+	.controller('LiveStreamController', ['$scope', function ($scope) {
+        $scope.loadVideo = function() {
+              if(Hls.isSupported()) {
+                var video = document.getElementById('video');
+                var hls = new Hls();
+                hls.loadSource('http://delta-1-yanexx65s8e5.live.elementalclouddev.com/out/p/9/it%20me.m3u8');
+                hls.attachMedia(video);
+                hls.on(Hls.Events.MANIFEST_PARSED,function() {
+                  video.play();
+              });
+            }
+        }
+}]);
