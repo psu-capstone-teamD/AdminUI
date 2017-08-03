54892faacd06ba03c96b7db817480c05c97d5977
Tyler Burdsall <tylerburdsall@gmail.com>
2017-08-02 19:01:10 -0700

    URL and fileName can be transferred between forms

--- app/controllers/MediaAssetsController.js
+++ app/controllers/MediaAssetsController.js
@@ -3,6 +3,8 @@ angular.module('adminUI')
 	.controller('MediaAssetsController', ['$scope', 'S3Service', '$q', 'mediaAssetsService', function ($scope, S3Service, $q, mediaAssetsService) {
     $scope.mediaAssets = mediaAssetsService.mediaAssets;
     $scope.S3Objects = [];
+    $scope.currentURL = "";
+    $scope.currentFileName = "";
 		
 	$scope.retrieveS3Objects = function(){
         $scope.mediaAssets = mediaAssetsService.mediaAssets;
@@ -18,4 +20,9 @@ angular.module('adminUI')
             }
         });
     };
+
+    $scope.updateCurrentS3Video = function(fileName, fileURL) {
+        $scope.currentFileName = fileName;
+        $scope.currentURL = fileURL;
+    }
 }]);
