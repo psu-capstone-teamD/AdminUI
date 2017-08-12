915f018fb73e632e742483414a88fc7afbcb6654
mukhsia <mukhsia@pdx.edu>
2017-08-11 19:40:23 -0700

    Made it so that the video doesn't play automatically when it loads.

--- clientapp/controllers/LiveStreamController.js
+++ clientapp/controllers/LiveStreamController.js
@@ -8,7 +8,6 @@ angular.module('clientUI')
                 hls.loadSource('http://delta-1-yanexx65s8e5.live.elementalclouddev.com/out/p/9/it%20me.m3u8');
                 hls.attachMedia(video);
                 hls.on(Hls.Events.MANIFEST_PARSED,function() {
-                  video.play();
               });
             }
         }
