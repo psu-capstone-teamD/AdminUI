0b4e2b6c9478dc0ebb5e28be0860a2a6d1ac6ef9
mukhsia <mukhsia@pdx.edu>
2017-08-12 20:17:30 -0700

    Made changes to clientUI LiveStreamController.
    -Renamed to ClientLiveStreamController to remove ambiguity with
    adminUI's LiveStreamController.
    -Updated the controller to match Tyler Burdsall's AdminUI Branch
    version.

--- clientapp/controllers/ClientLiveStreamController.js
+++ clientapp/controllers/ClientLiveStreamController.js
@@ -1,14 +1,18 @@
-// Define the LiveStreamController on clientUI module
+// Define the ClientLiveStreamController on clientUI module
 angular.module('clientUI')
-	.controller('LiveStreamController', ['$scope', function ($scope) {
+	.controller('ClientLiveStreamController', ['$scope', function ($scope) {
+        $scope.skipHlsCheck = false;
         $scope.loadVideo = function() {
-              if(Hls.isSupported()) {
+              if(($scope.skipHlsCheck === false) || (Hls.isSupported() !== undefined)) {
                 var video = document.getElementById('video');
                 var hls = new Hls();
                 hls.loadSource('http://delta-1-yanexx65s8e5.live.elementalclouddev.com/out/p/9/it%20me.m3u8');
                 hls.attachMedia(video);
                 hls.on(Hls.Events.MANIFEST_PARSED,function() {
               });
+            }
+            else {
+              toastr.error("Your browswer does not support HLS streaming", "Error");
             }
         }
 }]);
