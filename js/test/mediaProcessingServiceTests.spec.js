eb789ebf6671d2709b0cbd79cdaa4b0584e587c8
mukhsia <mukhsia@pdx.edu>
2017-08-21 08:09:27 -0700

    Added branch coverage to mediaProcessingServiceTests for coverall coverage.

--- js/test/mediaProcessingServiceTests.spec.js
+++ js/test/mediaProcessingServiceTests.spec.js
@@ -42,6 +42,46 @@ describe('mediaProcessingService', function(){
 				console.log("Unable to generate the duration");
 			});
 		});
+		it('should have 01:01:01 duration', function() {
+			mockVideo = {file:[{name:"testFile.mp4", 
+							size:1024, 
+							type:"video/mp4", 
+							duration:0,
+							id: "",
+							style: {},
+							src: ""
+						}]};
+			var findDuration = mediaProcessingService.findDuration(mockVideo, false);
+			spyOnEvent(mediaProcessingService.video, 'onloadedmetadata');
+			$(mediaProcessingService.video).trigger('onloadedmetadata');
+			findDuration.then(function() {
+				$(mediaProcessingService.video).trigger('onloadedmetadata');
+				expect($rootScope.videoLength).toBe(0);
+				expect($rootScope.fileDuration).toBe("00:00:00");
+			}, function(error) {
+				console.log("Unable to generate the duration");
+			});
+		});
+		it('should have 00:00:00 duration', function() {
+			mockVideo = {file:[{name:"testFile.mp4", 
+							size:1024, 
+							type:"video/mp4", 
+							duration:7201,
+							id: "",
+							style: {},
+							src: ""
+						}]};
+			var findDuration = mediaProcessingService.findDuration(mockVideo, false);
+			spyOnEvent(mediaProcessingService.video, 'onloadedmetadata');
+			$(mediaProcessingService.video).trigger('onloadedmetadata');
+			findDuration.then(function() {
+				$(mediaProcessingService.video).trigger('onloadedmetadata');
+				expect($rootScope.videoLength).toBe(7201);
+				expect($rootScope.fileDuration).toBe("01:01:01");
+			}, function(error) {
+				console.log("Unable to generate the duration");
+			});
+		});
 		it('should successfully find the duration with isFromS3 set to true', function() {
 			var findDuration = mediaProcessingService.findDuration("foo.com", true);
 			findDuration.then(function() {
